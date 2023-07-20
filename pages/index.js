86d91752a18e4db17b8fcf52ee7cfcfa7f111ed4
sagarjaid <69151227+sagarjaid@users.noreply.github.com>
2023-07-20 14:09:18 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-4xl font-semibold m-=auto flex justify-center items-center h-screen'>This is POC blog</div>
+  )
+}
+
+export default Home
